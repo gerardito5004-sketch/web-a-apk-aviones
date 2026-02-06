9af83fb23c49b62bca74cff7c60ca4f76d21bb86
gerardito5004-sketch <gerardito5004@gmail.com>
2026-02-05 23:23:41 -0400

    subiendo archivos
    subiendo archivos

---
+++ main.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+});
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
